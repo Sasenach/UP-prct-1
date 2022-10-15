029faddc8d02b4e7ae1f85309f15d6c33fe2f7b5
Sasenach <petrash123321@mail.ru>
2022-10-15 15:16:41 +0300

    first commit

---
+++ src/main/java/com/example/Uprct1/UPrct1Application.java
@@ -0,0 +1,13 @@
+package com.example.Uprct1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPrct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UPrct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Uprct1/UPrct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uprct1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPrct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
